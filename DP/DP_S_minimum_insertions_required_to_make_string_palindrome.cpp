91f00c9bc931358812848f435b35af7a8afba641
hercules09 <pmgupta09@gmail.com>
2022-08-25 20:08:47 +0530

    added edit

--- DP/DP_S_minimum_insertions_required_to_make_string_palindrome.cpp
+++ DP/DP_S_minimum_insertions_required_to_make_string_palindrome.cpp
@@ -8,4 +8,32 @@ using namespace std;
 
 //minimum insertions= length of the string - longest palindromic subsequence
 
+int lcs(string s1, string s2)
+{
+    int n=s1.length();
+    int m=s2.length();
+    vector<vector<int>> dp(n+1,vector<int> (m+1,0));
+    for(int j=0;j<=m;j++)
+    dp[0][j]=0;
+    for(int i=0;i<=n;i++)
+    dp[i][0]=0;
 
+    for(int i=1;i<=n;i++){
+        for(int j=1;j<=m;j++){
+            if(s1[i-1]==s2[j-1])                            
+             dp[i][j]= 1+dp[i-1][j-1];             
+    else dp[i][j]=max(dp[i-1][j],dp[i][j-1]);
+        }
+    }
+    return dp[n][m];
+}
+int longestPalindromeSubsequence(string s)
+{   string s1=s;
+    reverse(s1.begin(),s1.end());
+    return lcs(s, s1);
+ 
+}
+int minInsertion(string &str)
+{
+    return str.length()-longestPalindromeSubsequence(str);
+}
